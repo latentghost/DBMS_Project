33676d481c52d9f53230b2f9d37407043cc99a66
latentghost <akshat21515@iiitd.ac.in>
2023-03-26 17:45:30 +0530

    cli

--- cli.py
+++ cli.py
@@ -30,13 +30,13 @@ while(True):
 trigger1 = """
 DROP TRIGGER IF EXISTS check_stock;
 CREATE TRIGGER check_stock
-BEFORE INSERT ON Cart_Product
+INSTEAD OF INSERT ON Cart_Product
 FOR EACH ROW
 BEGIN
-    IF (SELECT p.Available_Stock FROM Product p WHERE p.Product_ID = NEW.Product_ID) < NEW.Quantity THEN
+    IF (SELECT Available_Stock FROM Product WHERE Product_ID = NEW.Product_ID) < NEW.Quantity THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "Not enough stock";
     ELSE
-        INSERT INTO `Cart_Product` (`Customer_ID`, `Product_ID`, `Quantity`) VALUES (NEW.Customer_ID, NEW.Product_ID, NEW.Quantity);
+        UPDATE Product SET Available_Stock = Available_Stock - NEW.Quantity WHERE Product_ID = NEW.Product_ID;
     END IF;
 END;"""
 
@@ -46,15 +46,15 @@ CREATE TRIGGER check_username
 BEFORE UPDATE ON Person
 FOR EACH ROW
 BEGIN
-    CASE
-        WHEN LENGTH(NEW.Username)<10 THEN
-            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Username too short';
-        WHEN NEW.Username = OLD.Username THEN
-            SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'New Username must be different from Old Username';
-        WHEN NEW.Username != OLD.Username AND EXISTS (SELECT * FROM Person WHERE Username = NEW.Username) THEN
-            SIGNAL SQLSTATE '45002' SET MESSAGE_TEXT = 'Username already exists, please try a different one';
-        
-    END CASE;
+    IF LENGTH(NEW.Username)<10 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Username too short';
+    END IF;
+    IF NEW.Username = OLD.Username THEN
+        SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'New Username must be different from Old Username';
+    END IF;
+    IF NEW.Username != OLD.Username AND EXISTS (SELECT * FROM Person WHERE Username = NEW.Username) THEN
+        SIGNAL SQLSTATE '45002' SET MESSAGE_TEXT = 'Username already exists, please try a different one';
+    END IF;
 END;"""
 
 
@@ -214,5 +214,6 @@ while(True):
         print("Invalid input")
 
 
+db.commit()
 cur.close()
 db.close()
