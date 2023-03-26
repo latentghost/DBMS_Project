02d4d53e72958593d56a908c4a509916fc0aeccc
Aditya Raj <96622681+rajaditya1306@users.noreply.github.com>
2023-03-26 15:07:47 +0530

    debugged queries

--- olap.sql
+++ olap.sql
@@ -1,5 +1,5 @@
 -- category wise and product wise sales
-SELECT c.Category_ID, c.Product_ID, c.pName, Sum(ps.ProductSales) AS TotalSales
+SELECT c.Category_ID AS Category_ID, c.Product_ID AS Product_ID, c.pName AS pName, Sum(ps.ProductSales) AS TotalSales
 FROM (
     SELECT Product_ID, Sum(Price) AS ProductSales 
     FROM Product_Sales 
@@ -12,13 +12,13 @@ INNER JOIN (
     ON Product.Product_ID = Product_PCategory.Product_ID
 ) c
 ON ps.Product_ID = c.Product_ID
-GROUP BY ROLLUP(c.Category_ID, c.Product_ID);
+GROUP BY Category_ID, Product_ID, pName WITH ROLLUP;
 
 -- slice counts as another operation so maybe we could just show the above but for a particular category using cube
 -- that's such copium though
 
 -- completed orders per state then city and so on
-SELECT ad.State, ad.City, ad.Street_Name, Count(*) AS TotalOrders
+SELECT ad.State AS State, ad.City AS City, ad.Street_Name AS Street_Name, Count(*) AS TotalOrders
 FROM Delivery_Person_Completed_Delivery dc
 INNER JOIN (
     SELECT cd.State, cd.City, cd.Street_Name, Orderr.Order_ID
@@ -27,4 +27,4 @@ INNER JOIN (
     ON Orderr.Customer_ID = cd.Customer_ID
 ) ad
 ON dc.Order_ID = ad.Order_ID
-GROUP BY ROLLUP(ad.State, ad.City, ad.Street_Name);
+GROUP BY State, City, Street_Name WITH ROLLUP;
